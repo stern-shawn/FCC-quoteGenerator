58327ce67f8af6f77fe670eb4d8ce242a178ca18
Shawn Stern <stern.shawn@gmail.com>
2016-08-28 19:31:29 -0700

    Merging changes between test and gh-pages branches

--- quoteApp.js
+++ quoteApp.js
@@ -14,12 +14,8 @@ $(document).ready(function() {
 */
 
 // Store our json object url in one short variable for cleaner code
-<<<<<<< HEAD
-jsonUrl = "http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?";
-=======
 // jsonUrl = "https://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?";
 jsonUrl = "https://wisdomapi.herokuapp.com/v1/random?jsonp=?";
->>>>>>> test
 
 // Factor out the getQuote action so we can automatically call it on page load, and manually on clicking the new quote button
 var getQuote = function(json) {
